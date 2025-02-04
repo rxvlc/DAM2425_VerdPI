626bc3f7595e0d3fd8c60a3e287fc03d731ef341
sergiosamper <sergiosamper.c@gmail.com>
2025-02-04 19:44:42 +0100

    reorganizacion codigo

--- front/StudX/src/App.js
+++ front/StudX/src/App.js
@@ -1,8 +1,8 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Login from "./screens/Login";
-import Registration from "./screens/Registration";
+import Login from "./screens/Inicio/Login";
+import Registration from "./screens/Inicio/Registration";
 
 
 
