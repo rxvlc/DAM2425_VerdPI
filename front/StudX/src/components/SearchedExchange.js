e834b5c23d299832058ec88c96c8fed4ff78b376
rxvlc <rngirona1@gmail.com>
2025-02-16 12:02:20 +0100

    Modifying component SearchedExchange

--- front/StudX/src/components/SearchedExchange.js
+++ front/StudX/src/components/SearchedExchange.js
@@ -1,12 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
 
 export default function SearchedExchange({ exchange }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{exchange.university}</Text>
-      <Text>{exchange.educationalLevel} - {exchange.academicLevel}</Text>
-      <Text>Estudiantes: {exchange.quantityStudents}</Text>
-      <Text>Estado: {exchange.status}</Text>
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{exchange.university}</Text>
+        <Text>{exchange.educationalLevel} - {exchange.academicLevel}</Text>
+        <Text>Estudiantes: {exchange.quantityStudents}</Text>
+        <Text>Estado: {exchange.status}</Text>
+      </View>
+      <TouchableOpacity
+        style={[styles.button, styles.chatButton]}
+        onPress={null}
+        activeOpacity={0.7}
+      >
+        <View style={styles.buttonContent}>
+          <Ionicons name="chatbubble-outline" size={16} color="white" />
+          <Text style={styles.buttonText}>Chat</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -22,9 +35,39 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 4,
     elevation: 2,
+    flexDirection: 'row',
+    justifyContent: 'space-between',  // Alinea el contenido de manera que el texto esté a la izquierda y el botón a la derecha
+    alignItems: 'center', // Asegura que todo el contenido esté centrado verticalmente
+  },
+  textContainer: {
+    flex: 1, // Permite que el texto ocupe el espacio disponible
   },
   title: {
     fontWeight: 'bold',
     fontSize: 16,
+  },
+  button: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 7,
+    paddingHorizontal: 20,
+    borderRadius: 25,
+    elevation: 2,
+  },
+  chatButton: {
+    backgroundColor: "#1E88E5",
+  },
+  solicitudButton: {
+    backgroundColor: "#FF5722",
+  },
+  buttonContent: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 12,
+    marginTop: 2, 
   },
 });
